66df6174668b5a573ac1c09278439d474ade2d79
Paulino <paulinomendes03@gmail.com>
2025-03-19 20:48:24 -0300

    Inicio do projeto

---
+++ src/main/java/dio/api/ApiRestNuvemApplication.java
@@ -0,0 +1,13 @@
+package dio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNuvemApplication.class, args);
+	}
+
+}
